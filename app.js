2badb089194ec3c20034316bfd988ba2c8857d7e
Anya Melnyk <gannasachakov@gmail.com>
2015-04-18 16:00:42 -0400

    before moving drop box to articles page

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 app.use(methodOverride('_method'));
+app.use("/styles", express.static(__dirname + '/styles'));
+app.use(express.static(__dirname + '/public'));
 //////////////////////////////////////// boiler plate
 
 
@@ -30,7 +32,8 @@ app.get('/', function(req, res) {
 	var template = fs.readFileSync('index.html', 'utf8');
 	db.all('SELECT * FROM authors;', function(err, authors) {
 		var html = Mustache.render(template, {
-			allAuthors: authors}); //end of mustache
+			allAuthors: authors
+		}); //end of mustache
 		//res.send("ok");
 		res.send(html);
 	}); //end of db all
@@ -61,26 +64,6 @@ app.get('/authors', function(req, res) {
 		res.send(html);
 	});
 });
-
-
-
-//if user wants to see authors profile with articles through the drop menu
-
-// app.get('/authors', function(req, res) {
-// 	var id = req.params.id;
-// 	var template = fs.readFileSync('./authors.html', 'utf8');
-
-// 	db.all('SELECT * FROM authors;', function(err, authors) {
-// 		var html = Mustache.render(template, {
-// 			allAuthors: authors
-// 		});
-// 		//res.send("ok");
-// 			console.log(req.query.authors);
-// 			res.send(req.query.authors);
-// 		//res.send(html);
-// 	});
-// });
-
 
 // app.get('/authors/:id', function(req, res) {
 // 	var username = req.query.authors;
@@ -136,13 +119,14 @@ app.put('/authors/:id/', function(req, res) {
 
 //reading the template from newarticle to create an article
 app.get('/articles/new', function(req, res) {
-		var template = fs.readFileSync('newarticle.html', 'utf8');
+	var template = fs.readFileSync('newarticle.html', 'utf8');
 	//res.send(template);
 	//sending new authors to a dropdown menu
 
 	db.all('SELECT * FROM authors;', function(err, authors) {
 		var html = Mustache.render(template, {
-			allAuthors: authors}); //end of mustache
+			allAuthors: authors
+		}); //end of mustache
 		//res.send("ok");
 		res.send(html);
 	}); //end of db all
@@ -156,25 +140,86 @@ app.post('/articles', function(req, res) {
 	var category = req.body.categories;
 	var author = req.body.authors;
 	console.log(author);
-	db.run("INSERT INTO articles (category, title, content, date_created, image, authors_id) VALUES ('" + category + "','" + req.body.title + "','" + req.body.content + "','" + req.body.date_created + "','" + req.body.image + "','" + author+ "')");
+	db.run("INSERT INTO articles (category, title, content, date_created, image, authors_id) VALUES ('" + category + "','" + req.body.title + "','" + req.body.content + "','" + req.body.date_created + "','" + req.body.image + "','" + author + "')");
 	//res.send('article added');
 	console.log('article info sent to database');
 	res.redirect("/articles");
 
 });
 
+
 //view all articles. reading a template and mustaching articles into html
 app.get('/articles', function(req, res) {
 	var template = fs.readFileSync('./articles.html', 'utf8');
+	var allAuthors = "SELECT * FROM authors;";
+	var allArticles = "SELECT * FROM articles;";
+	// var authorNameSearchedInArticles = "SELECT title FROM articles WHERE authors_id IN (SELECT id FROM authors WHERE username = 'test');";
+	var authorSearch = req.query.authors;
+	console.log(authorSearch);
 
-	db.all('SELECT * FROM articles;', function(err, articles) {
+	var authorNameSearchedInArticles = "SELECT * FROM articles WHERE authors_id = " + authorSearch + ";";
+	//console.log(authorNameSearchedInArticles);
+	//console.log("hello");
+	if (authorSearch) {
+		// 	//accessing ALL articles of a particular user
+		db.all(authorNameSearchedInArticles, function(err, articles) {
+			var html = Mustache.render(template, {
+				allArticles: articles});
+			//res.send("ok");
+			res.send(html);
+		}); //end of db all nested
+	} //end of if statement
+	else {
+		// 	//accessing ALL articles of ALL users
+	db.all(allArticles, function(err, articles) {
 		var html = Mustache.render(template, {
 			allArticles: articles
 		});
 		//res.send("ok");
 		res.send(html);
-	});
-});
+	}); //end of db all nested
+
+// 	// END OF ACCESSING ALL ARTICLES	
+	}
+}); //supposedly end of app get
+
+// 	// END OF ACCESSING ALL ARTICLES	
+// db.all(allAuthors, {}, function(err, authors) {
+// 	authors.forEach(function(authorTableElement) {
+// 							console.log(req.body.authors);
+// if (req.body.authors === authorTableElement.id) {
+// if (req.body.authors) {
+
+
+
+
+
+// db.all(authorNameSearchedInArticles, {}, function(err, data) {
+// 	data.forEach(function(articlesTableElement) {
+// 		console.log(authorTableElement.id + " wrote " + articlesTableElement.title);
+
+// 	}); //end of forEach nested
+// }); //end of db all nested 
+// var html = Mustache.render(template, {
+// 	allArticles: articles
+// });
+//res.send("ok");
+//res.send(html);
+// } //end of if
+// else {
+
+
+// } //end of else
+//	}); // end of forEach
+//	}); //end of db.all
+//res.send(html);
+//res.send("bla");
+
+
+//the good part
+// + req.body.authors +
+//if user wants to see authors profile with articles through the drop menu
+
 
 
 //editing or deleting article

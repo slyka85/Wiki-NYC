01ae9299b325ad27da0bcb2ac504daf283a248b7
Anya Melnyk <gannasachakov@gmail.com>
2015-04-19 19:39:21 -0400

    before deployment

--- app.js
+++ app.js
@@ -9,14 +9,22 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var db = new sqlite3.Database('./database.db');
+var path = require('path');
+var marked = require('marked');
+//var sendgrid = require("sendgrid")(api_user, api_key);
+//var email = new sendgrid.Email();
+
 var app = express();
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
-app.use("/styles", express.static(__dirname + '/styles'));
-app.use(express.static(__dirname + '/public'));
+app.use("/styles", express.static(__dirname + '/public/css'));
+// app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/views'));
+// app.use(express.static(__dirname + '/uploads'));
+//app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(__dirname + '/'));
+//app.use(express.static('/views'));
 //////////////////////////////////////// boiler plate
 
 
@@ -29,7 +37,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
 	//sending new authors to a dropdown menu
-	var template = fs.readFileSync('index.html', 'utf8');
+	var template = fs.readFileSync('./views/index.html', 'utf8');
 	db.all('SELECT * FROM authors;', function(err, authors) {
 		var html = Mustache.render(template, {allAuthors: authors}); //end of mustache
 		res.send(html);
@@ -50,7 +58,7 @@ app.post('/authors', function(req, res) {
 
 //view all authors. reading a template and mustaching authors into html
 app.get('/authors', function(req, res) {
-	var template = fs.readFileSync('./authors.html', 'utf8');
+	var template = fs.readFileSync('./views/authors.html', 'utf8');
 
 	db.all('SELECT * FROM authors;', function(err, authors) {
 		var html = Mustache.render(template, {
@@ -60,20 +68,17 @@ app.get('/authors', function(req, res) {
 	});
 });
 
-//viewing a particular author
+//accessing a page of a particular author to view
 app.get('/authors/:id', function(req, res) {
 	var id = req.params.id;
 	db.all('SELECT * FROM authors WHERE id= ' + id + ";", function(err, author) {
-				// fs.readFile('./authorPage2.html', 'utf8', function(err, html) {
-		fs.readFile('./authorPage2.html', 'utf8', function(err, html) {
-			console.log(author);
+		fs.readFile('./views/authorPage2.html', 'utf8', function(err, html) {
+			//console.log(author);
 			var renderedHTML = Mustache.render(html, author[0]);
 			res.send(renderedHTML);
-		});
-	});
-});
-
-
+		}); //end of fa readFile
+	}); //end of db all
+}); //end of app get authors id
 
 //to edit a particular author
 app.get('/authors/:id/edit', function(req, res) {
@@ -83,7 +88,7 @@ app.get('/authors/:id/edit', function(req, res) {
 
 	db.all('SELECT * FROM authors WHERE id= ' + id + ";", function(err, author) {
 				// fs.readFile('./authorPage2.html', 'utf8', function(err, html) {
-		fs.readFile('./authorPageEdit.html', 'utf8', function(err, html) {
+		fs.readFile('./views/authorPageEdit.html', 'utf8', function(err, html) {
 			console.log(author);
 			var renderedHTMLEdit = Mustache.render(html, author[0]);
 			//res.send(id);
@@ -99,13 +104,14 @@ app.delete('/authors/:id', function(req, res) {
 	res.redirect("/authors");
 });
 
-//editing the authors profile
+//sending changes of the author to a database
 app.put('/authors/:id/', function(req, res) {
 	var id = req.params.id;
 	var authorInfo = req.body;
 	db.run("UPDATE authors SET username = '" + authorInfo.username + "',first_name = '" + authorInfo.first_name + "', last_name = '" + authorInfo.last_name + "', email = '" + authorInfo.email + "', profile_image = '" + authorInfo.profile_image + "' WHERE id = " + id + ";");
-	res.redirect("/authors/:id");
+	res.redirect("/authors");
 });
+
 
 ///////////////////////////////////////////////////////////////////////////
 //                 ARTICLE STUFF                                        //
@@ -113,7 +119,7 @@ app.put('/authors/:id/', function(req, res) {
 
 //reading the template from newarticle to create an article
 app.get('/articles/new', function(req, res) {
-	var template = fs.readFileSync('newarticle.html', 'utf8');
+	var template = fs.readFileSync('./views/newarticle.html', 'utf8');
 
 	//sending new authors to a dropdown menu
 	db.all('SELECT * FROM authors;', function(err, authors) {
@@ -137,7 +143,7 @@ app.post('/articles', function(req, res) {
 
 //view all articles. reading a template and mustaching articles into html
 app.get('/articles', function(req, res) {
-	var template = fs.readFileSync('./articles.html', 'utf8');
+	var template = fs.readFileSync('./views/articles.html', 'utf8');
 	var allAuthors = "SELECT * FROM authors;";
 	var allArticles = "SELECT * FROM articles;";
 	var authorSearch = req.query.authors;
@@ -162,11 +168,11 @@ app.get('/articles', function(req, res) {
 	// END OF ACCESSING ALL ARTICLES	
 }); // end of app get
 
-//accessing page to edit an article
+//accessing article page to edit an article
 app.get('/articles/:id', function(req, res) {
 	var id = req.params.id;
 	db.all('SELECT * FROM articles WHERE id= ' + id + ";", function(err, article) {
-		fs.readFile('./articlePage.html', 'utf8', function(err, html) {
+		fs.readFile('./views/articlePage.html', 'utf8', function(err, html) {
 
 			var renderedHTML = Mustache.render(html, article[0]);
 			console.log(article);
@@ -179,7 +185,7 @@ app.delete('/articles/:id', function(req, res) {
 	var id = req.params.id;
 	db.run("DELETE FROM articles WHERE id =" + id + ";");
 	res.redirect("/articles");
-});
+}); //end of app delete
 
 //sending changes to a server
 app.put('/articles/:id/', function(req, res) {
@@ -187,7 +193,7 @@ app.put('/articles/:id/', function(req, res) {
 	var articleInfo = req.body;
 	db.run("UPDATE articles SET category = '" + articleInfo.category + "', title = '" + articleInfo.title + "', content = '" + articleInfo.content + "', date_created = '" + articleInfo.date_created + "', image = '" + articleInfo.image + "', authors_id = '" + articleInfo.authors_id + "' WHERE id = " + id + ";");
 	res.redirect("/articles");
-});
+}); //end of app put
 
 
 app.listen(5000, function() {
